a898a3c17c0683c8fa4160bd0e0efa6886896e4f
Михаил Никитин <misha@mrsunstrike.ru>
2023-07-05 11:54:36 +0700

    Создана новая модель адресов и миграции для нее

--- address_standardizer/api/models.py
+++ address_standardizer/api/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class AddressStandardization(models.Model):
+    request = models.CharField(max_length=60)
+    result = models.TextField(blank=True, null=True)
