659b54eb4170dc6207dc1b20cba886f282868ccc
Михаил Никитин <misha@mrsunstrike.ru>
2023-07-05 12:36:12 +0700

    Добавил версионность API через роутер в urls

--- address_standardizer/api/urls.py
+++ address_standardizer/api/urls.py
@@ -1,6 +1,10 @@
 from api.views import *
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+router_v1 = DefaultRouter()
+# router_v1.register('address', APIAddressView, basename='address')
 
 urlpatterns = [
-    path('v1/address/', ...),
+    path('v1/', include(router_v1.urls)),
 ]
